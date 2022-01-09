a058dba408813ecf7028a2ae6d313b13e1adc20c
omarsayed12 <omarsayed40400@gmail.com>
2022-01-09 02:03:55 +0200

    add menu page and creat log-in and registration page and creat shopping cart

---
+++ assets/js/log-in.js
@@ -0,0 +1,221 @@
+// define global variable
+let emailArray=[];
+let passwordArray=[];
+let alertForgot = document.querySelector(".alert-forgot");
+let alertForgotPass = document.querySelector(".alert-forgot-pass");
+
+// login and register variable define
+let regTab = document.getElementById("btnRregister");
+let loginTab = document.getElementById("btnLogin");
+
+//form sign in variable define
+let signInForm = document.getElementById("login");
+let email = document.getElementById("logMail");
+let password = document.getElementById("logPass");
+let btnSubmit = document.getElementById("submit");
+
+// form register variable define
+let registerForm = document.getElementById("register");
+let emailReg = document.getElementById("reMail");
+let passwordReg = document.getElementById("repass");
+let passwordRetype = document.getElementById("rePassPlaceholder");
+let regSubmit = document.getElementById("regSubmit");
+
+// form forgot mail variable define
+let forgetRgisterForm = document.getElementById("forgot");
+let forgotMail = document.getElementById("forgetMail");
+let submitForgotPass = document.getElementById("forgetPass");
+
+
+// function to change style register button
+regTab.addEventListener("click", (event) => {
+
+    registerForm.style.visibility="visible";
+    signInForm.style.visibility="hidden";
+    forgetRgisterForm.style.visibility="hidden";
+
+    regTab.style.backgroundColor="#72a73e";
+    loginTab.style.backgroundColor="#FFF";
+    regTab.style.color = "#FFF";
+    loginTab.style.color="#000";
+
+})
+
+// function to change style log in button
+loginTab.addEventListener("click", (event)=> {
+    registerForm.style.visibility="hidden";
+    signInForm.style.visibility="visible";
+    forgetRgisterForm.style.visibility="hidden";
+
+    loginTab.style.backgroundColor="#72a73e";
+    regTab.style.backgroundColor="#FFF";
+    regTab.style.color = "#000";
+    loginTab.style.color="#FFF";
+})
+
+// function to check forget pass
+submitForgotPass.addEventListener("click", (event)=> {
+  registerForm.style.visibility="hidden";
+  signInForm.style.visibility="hidden";
+  forgetRgisterForm.style.visibility="visible";
+  loginTab.style.backgroundColor="#72a73e";
+})
+
+// function event Sign In
+btnSubmit.addEventListener("click", (event)=> {
+  event.preventDefault();
+
+  let i = emailArray.indexOf(email);
+  if(document.documentElement.lang === "ar") {
+    if(emailArray.indexOf(email) === -1){
+      if (email.value === "") {
+        alertForgot.textContent = "من فضلك ادخل البريد الالكتروني";
+        return ;
+      }
+    }
+    // else if(passwordArray[i] !== password){
+    //   if (password.value === ""){
+    //     alertForgotPass.textContent = "ادخل كلمه المرور";
+    //     return ;
+    //   }
+    //   alertForgotPass.textContent = "كلمه المرور لا تطابق"
+    //   return ;
+    // }
+  } else if (document.documentElement.lang === "en") {
+      if(emailArray.indexOf(email) === -1){
+      if (email.value === "") {
+        alertForgot.textContent = "Please enter your email";
+        return ;
+      }
+    }
+    else if(passwordArray[i] !== password){
+      if (password.value === ""){
+        alertForgotPass.textContent = "enter password"
+        return ;
+      }
+      alertForgotPass.textContent = "Password does not match"
+      return ;
+    }
+  }
+  check();
+  password.value = "";
+  email.value = "";
+})
+regSubmit.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  var validForm = false;
+  if(emailReg.value  !== "" && passwordReg.value !== ""&&
+    passwordRetype.value !== "" &&
+    passwordReg.value === passwordRetype.value &&
+    emailArray.indexOf(emailReg) !== -1) {
+
+      validForm = true;
+  }
+
+  if(validForm) {
+    localStorage.setItem("email", emailReg.value);
+    localStorage.setItem("pass", passwordReg.value);
+    localStorage.setItem("passRetype", passwordRetype.value);
+    window.location.href = "../index.html";
+  }
+
+  if(document.documentElement.lang === "ar") {
+    if (emailReg.value === ""){
+    let alertForgotReges = document.querySelector(".alert-forgot-reges");
+    alertForgotReges.textContent = "من فضلك ادخل البريد لالكتروني";
+      return ;
+    }
+    else if (passwordReg.value === ""){
+      let alertForgotPass = document.querySelector(".alert-forgot-regesPass");
+      alertForgotPass.textContent = "ادخل كلمه المرور"
+      return ;
+    }
+    else if (passwordRetype.value == ""){
+      let alertForgotReRegesPass  = document.querySelector(".alert-forgot-resRegesPass");
+      alertForgotReRegesPass.textContent = "تأكيد ادخال كلمه المرور"
+      return ;
+    }
+    else if ( passwordReg.value != passwordRetype.value ){
+      alert("كلمة المرور لا تطابق كلمة السر الخاصة بك");
+      return;
+    }
+    else if(emailArray.indexOf(emailReg) == -1){
+      emailArray.push(emailReg);
+      passwordArray.push(passwordReg);
+      // window.Location.href = "../index.html"
+    }
+  }
+  else if (document.documentElement.lang === "en") {
+    if (emailReg.value === "") {
+      let alertForgotReges = document.querySelector(".alert-forgot-reges");
+      alertForgotReges.textContent = "Please enter your email";
+        return ;
+    }else if (passwordReg.value === ""){
+      let alertForgotPass = document.querySelector(".alert-forgot-regesPass");
+      alertForgotPass.textContent = "enter password"
+      return ;
+    }else if (passwordRetype.value == ""){
+      let alertForgotReRegesPass  = document.querySelector(".alert-forgot-resRegesPass");
+      alertForgotReRegesPass.textContent = "Confirm password"
+      return ;
+    }else if ( passwordReg.value != passwordRetype.value ){
+      alert("Password don't match retype your Password");
+      return;
+    }else if(emailArray.indexOf(emailReg) == -1){
+      emailArray.push(emailReg);
+      passwordArray.push(passwordReg);
+      // window.Location.href = "../index.html"
+    }
+  }
+})
+
+forgot.addEventListener("click", (event) => {
+  event.preventDefault();
+  let forgotPassword = document.querySelector(".alert-forgot-password");
+
+  if(document.documentElement.lang === "ar") {
+    if(emailArray.indexOf(forgotMail.value) == -1){
+      if (forgotMail.value === ""){
+        forgotPassword.textContent = "من فضلك ادخل البريد الالكتروني";
+        return ;
+      }
+      forgotPassword.textContent = "البريد الاكتروني غير موجود";
+        return ;
+      }
+
+    } if(document.documentElement.lang === "en") {
+        if(emailArray.indexOf(forgotMail.value) == -1){
+      if (forgotMail.value === ""){
+        forgotPassword.textContent = "Please enter your email";
+        return ;
+      }
+      forgotPassword.textContent = "email not found";
+        return ;
+      }
+    }
+})
+
+function check() {
+  var storeMail = localStorage.getItem("email");
+  var storPass = localStorage.getItem("pass");
+
+  if(document.documentElement.lang === "ar") {
+    if(email.value == storeMail && password.value == storPass) {
+      window.location.href = "../index.html";
+    } else if(password.value === ""){
+      alertForgotPass.textContent = "كلمه المرور"
+    } else {
+      alertForgot.textContent  = "سجل اولا";
+    }
+
+  } if(document.documentElement.lang === "en") {
+    if(email.value == storeMail && password.value == storPass) {
+      window.location.href = "../home-page-en.html";
+    } else if(password.value === ""){
+      alertForgotPass.textContent = "enter Password";
+    } else {
+      alertForgot.textContent  = "Create an account";
+    }
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
+// define global variable
 let navbar = document.getElementById('fixed-nav');
+let header = document.getElementById('header');
+
+// function to do sticky navbar
 window.addEventListener("scroll", ()=> {
   setTimeout(() =>{
     if (window.scrollY >= 50) {
@@ -9,22 +13,133 @@ window.addEventListener("scroll", ()=> {
   })
 })
 
-
-let header = document.getElementById('header');
-
+//function to do style for the header
 window.addEventListener("scroll", () =>{
   setTimeout(()=> {
-  window.scrollY >=50
+  window.scrollY >= 50
     ? (header.style.marginTop = "80px")
     : (header.style.marginTop = "0")
   })
 });
 
+// function date picker
 
 
-$( function() {
-	$( "#datepicker" ).datepicker({
-		// dateFormat: "dd بدايه من يوم",
-		duration: "fast"
-	});
-} );
+let productInCart =JSON.parse(localStorage.getItem("shoppingCart"));
+const parentElement = document.querySelector("#buyItems");
+const cartSumPrice = document.querySelector("#sum-price");
+const products = document.querySelectorAll(".mealz-content");
+
+if(!productInCart) {
+  productInCart = [];
+}
+
+const countTheSumPrice = function() {
+  let sumPrice = 0;
+  productInCart.forEach(product => {
+    sumPrice += product.price
+  });
+  return sumPrice;
+}
+
+
+const updateShopingCartHTML = function() {
+  localStorage.setItem("shoppingCart",JSON.stringify(productInCart));
+
+  if(productInCart.length > 0) {
+    let result = productInCart.map(product => {
+      return `
+      <li class="buyItem">
+          <div>
+            <h5> ${product.name}</h5>
+            <h6>${product.price}$</h6>
+            <div>
+              <button class="button-minus btn link-light btn-info m-1 px-2 py-0" data-id='${product.id}'>-</button>
+              <span class="countOfProduct">${product.count}</span>
+              <button class="button-plus btn link-light btn-info px-2 py-0" data-id='${product.id}'>+</button>
+            </div>
+          </div>
+           <img src="${product.image}" class="w-100 img-product rounded">
+        </li>
+      `
+    });
+    parentElement.innerHTML = result.join('');
+    // document.querySelector(".checkout").classList.remove("hidden");
+    // cartSumPrice.innerHTML = "$" + countTheSumPrice();
+
+  } else {
+    // document.querySelector(".checkout").classList.add("hidden");
+    parentElement.innerHTML = '<h4 class="empty">shoping cart is impty</h4>';
+    cartSumPrice.innerHTML = "";
+  }
+}
+function updateProductsinCart(product) {
+  for(let i = 0; i < productInCart.length; i++) {
+    if(productInCart[i].id == product.id) {
+      productInCart[i].count += 1;
+      productInCart[i].price = productInCart[i].basePrice * productInCart[i].count;
+      return;
+    }
+  }
+  productInCart.push(product);
+}
+products.forEach(product => {
+  product.addEventListener("click", (e) => {
+    if(e.target.classList.contains('add-to-cart')) {
+      const productID = e.target.dataset.productId;
+      const productName = product.querySelector(".meal-name").innerHTML;
+      console.log(productName);
+      const productPrice = product.querySelector(".price-value").innerHTML;
+       console.log(productPrice);
+      const ProductImg = product.querySelector(".img-meal").src;
+      console.log(ProductImg);
+
+      let productToCart = {
+        name: productName,
+        image: ProductImg,
+        id: productID,
+        count: 1,
+        price: +productPrice,
+        basePrice : +productPrice
+      }
+      updateProductsinCart(productToCart);
+      updateShopingCartHTML();
+    }
+  })
+});
+
+parentElement.addEventListener('click', (e) => {
+  const isPlusButton = e.target.classList.contains('button-plus');
+  const isMinusButton = e.target.classList.contains('button-minus');
+  if(isPlusButton || isMinusButton) {
+    for(let i = 0; i <productInCart.length; i++) {
+      if(productInCart[i].id === e.target.dataset.id) {
+        if(isPlusButton) {
+          productInCart[i].count += 1;
+        } else if(isMinusButton) {
+          productInCart[i].count -= 1;
+        }
+        productInCart[i].price = productInCart[i].basePrice * productInCart[i].count;
+      }
+      if(productInCart[i].count <= 0) {
+        productInCart.splice(i, 1);
+      }
+    }
+    updateShopingCartHTML();
+  }
+})
+
+updateShopingCartHTML();
+
+let cart = document.querySelector('.cart-btn');
+cart.addEventListener("click", () => {
+  let disCart = document.querySelector('.cart');
+
+  if (disCart.style.display === "block") {
+    disCart.style.display = "none";
+  } else {
+   disCart.style.display = "block";
+  }
+})
+
+
